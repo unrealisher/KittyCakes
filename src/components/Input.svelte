e7a07e67038948c3c8be86c8c63fa7c768589f01
unrealisher <unrealisher96@yandex-team.ru>
2020-01-22 20:20:48 +0300

    Форма отправки письма

---
+++ src/components/Input.svelte
@@ -0,0 +1,16 @@
+<label>{label}
+    <input type='text' {placeholder} bind:value={value}>
+</label>
+
+<style></style>
+
+<script>
+    let params = {};
+    let value;
+    const { label, placeholder } = params;
+
+    export {
+        value,
+        params
+    };
+</script>
--- src/server.js
+++ src/server.js
@@ -1,17 +1,42 @@
 import sirv from 'sirv';
-import polka from 'polka';
+import express from 'express';
+import bodyParser from 'body-parser';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV } = process.env; // eslint-disable-line no-undef
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/get', (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
+
+	res.end(JSON.stringify({
+		hello: 'Hello world'
+	}))
+});
+
+app.post('/post', (req, res) => {
+	// const result = req.body;
+	//
+	// console.log(result);
+
+	res.setHeader('Content-Type', 'application/json');
+
+	res.end(JSON.stringify({
+		word: 'thx'
+	}));
+});
+
+app.use(
+	compression({ threshold: 0 }),
+	sirv('static', { dev }),
+	sapper.middleware()
+);
+
+app.listen(PORT, err => {
+	if (err) console.log('error', err); // eslint-disable-line no-console
+});
