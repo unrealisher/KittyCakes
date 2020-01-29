db2a7e31f701a4778206834497bcc2911e75cf81
unrealisher <unrealisher96@yandex-team.ru>
2020-01-29 19:54:29 +0300

    Объявление css переменных

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,9 +3,15 @@
 </svelte:head>
 
 <h1>KittyCakes</h1>
+<p>
+	Всем привет, на связи kittycakes и мы готовы начать путешествие в дальние страны к неведомым просторам рунета Всем привет,
+	на связи kittycakes и мы готовы начать путешествие в дальние страны к неведомым просторам рунета  Всем привет, на связи
+	kittycakes и мы готовы начать путешествие в дальние страны к неведомым просторам рунета Всем привет, на связи kittycakes
+	и мы готовы начать путешествие в дальние страны к неведомым просторам рунета
+</p>
 
 <FeedbackForm />
 
 <script>
-	import FeedbackForm from "../components/FeedbackForm.svelte";
+	import FeedbackForm from '../components/FeedbackForm.svelte';
 </script>
